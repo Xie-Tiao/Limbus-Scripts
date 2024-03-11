ae6e26bb4a8515fdf705eb642c6d98fa11b7d514
unknown <x_t>
2024-03-11 09:45:23 +0800

    formatting

--- workbench/ego_gift.py
+++ workbench/ego_gift.py
@@ -1,9 +1,14 @@
-import pyautogui as pg
-import file_path_utils
 import json
 import os
 import time
+
+import pyautogui as pg
+
+import file_path_utils
+
 pg.FAILSAFE = False
+
+
 # 界面检查模块
 def check_img(img, confid=0.9):
     try:
@@ -20,6 +25,7 @@ def check_img_list(img_list, confid=0.9):
         if check_img(img_list[i], confid):
             return True
     return False
+
 
 # 控制模块
 def mouse_click(img, times=1, confid=0.9):
@@ -46,34 +52,41 @@ def mouse_click_img_list(img_list, times=1, confid=0.9):
             return True
     return False
 
+
 _worklist_path = os.path.join(file_path_utils.PathManager.CURRENT_DIR, 'worklist.json')
 with open(_worklist_path, 'r', encoding='utf-8') as f:
     _worklist = json.load(f)
 
-##————————————————————————————————————————————
-##————————————————————————————————————————————
+
+# ————————————————————————————————————————————
+# ————————————————————————————————————————————
 def start_mirror():
     mouse_click_img_list(_worklist['start_mirror'])
+
 
 def end_mirror():
     mouse_click_img_list(_worklist['end_mirror'])
 
+
 def gift(camp):
     for _ in range(2):
-        camp_checked = check_img_list(_worklist[f'{camp}_checked'][0],0.85) & check_img_list(_worklist[f'{camp}_checked'][1],0.85)
+        camp_checked = check_img_list(_worklist[f'{camp}_checked'][0], 0.85) & check_img_list(
+            _worklist[f'{camp}_checked'][1], 0.85)
         if camp_checked:
-            mouse_click_img_list(_worklist[f'{camp}_click'],confid=0.85)
+            mouse_click_img_list(_worklist[f'{camp}_click'], confid=0.85)
             mouse_click('gifts_checked_1690.png')
             print('等待下一步操作')
             time.sleep(999999)
         else:
             mouse_click('refresh_1690.png')
 
+
 def mirror_opening_field():
     while True:
         start_mirror()
         gift('bleed')
         end_mirror()
+
 
 """
     流血: bleed
